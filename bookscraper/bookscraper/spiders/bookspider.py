d2a4bd0ac40d16cc9dc7c9ca39555e915a1c5d7a
Hritik kumar <hritikkumar2011@gmail.com>
2024-01-21 17:43:46 +0530

    correct run

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -1,24 +1,23 @@
 import scrapy
 
-
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        books=response.css('article.product_pod')
+        books = response.css('article.product_pod')
         for book in books:
             relative_url = book.css('h3 a ::attr(href)').get()
             if 'catalogue/' in relative_url:
-                book_url = 'https://books.toscrape.com/'+ relative_url
+                book_url = 'https://books.toscrape.com/' + relative_url
             else:
-                book_url = 'https://books.toscrape.com/catalogue/'+ relative_url
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
             yield response.follow(book_url, callback=self.parse_book_page)
 
-            print('1-------------------------------------------------------------------------------------------------------')
+        print('1-------------------------------------------------------------------------------------------------------')
         next_page = response.css('li.next a ::attr(href)').get()
-        
+
         if next_page is not None:
             if 'catalogue/' in next_page:
                 next_page_url = 'https://books.toscrape.com/' + next_page
@@ -27,21 +26,25 @@ class BookspiderSpider(scrapy.Spider):
                 next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
                 print('inside else catalogue----------------------------------------------------------------------------------')
 
-            yield response.follow(next_page_url, callback= self.parse)
+            # Check if the next page URL is within the allowed domain
+            if self.allowed_domains[0] in next_page_url:
+                yield response.follow(next_page_url, callback=self.parse)
+            else:
+                self.logger.warning(f"Skipping external domain: {next_page_url}")
 
     def parse_book_page(self, response):
-        table_rows = response. css("table tr")
+        table_rows = response.css("table tr")
         yield {
-            'url' : response.url,
-            'title': response.css('.product_main h1: :text').get(),
-            'product_type': table_rows [1].css("td ::text").get(),
+            'url': response.url,
+            'title': response.css('.product_main h1 ::text').get(),
+            'product_type': table_rows[1].css("td ::text").get(),
             'price_excl_tax': table_rows[2].css("td ::text").get(),
             'price_incl_tax': table_rows[3].css("td ::text").get(),
             'tax': table_rows[4].css("td ::text").get(),
-            'availability': table_rows [5].css("td ::text").get(),
-            'num_reviews': table_rows [6].css("td ::text").get(),
-            'stars' : response.css("p.star-rating").attrib['class'],
-            'category': response.xpath("//ul|@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),   
+            'availability': table_rows[5].css("td ::text").get(),
+            'num_reviews': table_rows[6].css("td ::text").get(),
+            'stars': response.css("p.star-rating").attrib['class'],
+            'category': response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
             'description': response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
             'price': response.css('p.price_color ::text').get(),
         }
